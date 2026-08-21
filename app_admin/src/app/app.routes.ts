d2cb1aa23deda68fe85a476aa638ec1279520eb9
Goerlitz Edmond <goerlitz.edmond@snhu.edu>
2026-08-21 14:04:17 -0400

    Module 7: Add auth guard, fix login template, secure Angular routes

--- app_admin/src/app/app.routes.ts
+++ app_admin/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { TripListingComponent } from './trip-listing/trip-listing';
 import { AddTripComponent } from './add-trip/add-trip';
 import { EditTripComponent } from './edit-trip/edit-trip';
 import { LoginComponent } from './login/login';
+import { AuthGuard } from './auth-guard';
 
 export const routes: Routes = [
   { path: '', component: TripListingComponent },
-  { path: 'add-trip', component: AddTripComponent },
-  { path: 'edit-trip', component: EditTripComponent },
+  { path: 'add-trip', component: AddTripComponent, canActivate: [AuthGuard] },
+  { path: 'edit-trip', component: EditTripComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
